1925fcaa94e5594084d0b0461e159aa2ddd9eb85
JohannMscs <johann.moscoso@gmail.com>
2024-02-29 09:59:00 -0300

    estilizando e criando algumas funções da ToDo-List

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 import { useState, useEffect } from 'react';
 import {BsTrash, BsBookmarkCheck, BsBookmarkCheckFill} from 'react-icons/bs'
 
-const API = "https://localhost:5000"
+const API = "http://localhost:5000/todos"
 
 function App() {
 
@@ -12,7 +12,24 @@ function App() {
   const [todos, setTodos] = useState([])
   const [loading, setLoading] = useState(false)
 
-  const handleSubmit = (e) =>{
+  useEffect(()=>{
+
+    const loadData = async() =>{
+      setLoading(true)
+
+      const resp = await fetch('http://localhost:5000/todos')
+      .then((resp) => resp.json())
+      .then((data) => data)
+      .catch((error) => console.log(error));
+      setLoading(false)
+      setTodos(resp)
+    };
+
+    loadData()
+  },[])
+  
+
+  const handleSubmit = async(e) =>{
     e.preventDefault();
     console.log("enviado");
     console.log(title)
@@ -22,10 +39,20 @@ function App() {
       title,
       time,
       done: false,
-    }
+    };
 
+   
+    //envia os dados digitados no input para o db.json
+    await fetch(API, {
+      method: "POST",
+      body: JSON.stringify(todo),
+     headers: {
+      "Content-Type": "application/json",
+     },
+
+    });
     console.log(todo)
-
+    console.log()
     setTitle("")
     setTime("")
   }
@@ -59,10 +86,17 @@ function App() {
 
         <div className="list_todo">
           <h2>Lista de Tarefas</h2>
+          <p></p>
           {todos.length === 0 && <p>Não há tarefas.</p>}
+          
+          {todos.map((todo) =>(
+            <div className='todo' key={todo.id}>
+              <p>{todo.title}</p>
+            </div>
+          ))}
+          
         </div>
       </section>
-
 
 
 
